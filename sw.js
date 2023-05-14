0e37267cefc5aa0566968371ef4a345aca254727
Kakid <Kakid@Kakids-MacBook-Pro.local>
2023-05-14 16:50:27 +0800

    PWA

---
+++ sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'tiddlywiki-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache initialized successfully');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          console.log('Cache hit');
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
